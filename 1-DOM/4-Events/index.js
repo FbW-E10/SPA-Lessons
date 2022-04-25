e50119f3a40d171f3fb069348302a34675659ec1
Ghassanooooo <ghassanaldarwish@gmail.com>
2022-04-25 15:56:24 +0200

    template events updated

--- 1-DOM/4-Events/index.js
+++
@@ -1,8 +0,0 @@
-// * THE EVENT OBJECT
-
-// When an event listener is triggered and calls its associated function, it automatically passes a single ARGUMENT to the function
-// This argument is a reference to the event object
-// The event object contains a number of properties that describe the event which occurred
-
-// ===============================================================================================
-//####### select the elements ##########
