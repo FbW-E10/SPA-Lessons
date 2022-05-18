a92bf5dc5dfa866e43ec01234fd0bddc992c7a75
Ghassan Aldarwish <ghassanaldarwish@gmail.com>
2022-05-18 11:58:26 +0200

    Update App.js

--- 5-Component/4-Data flow: Props & States definitions & difference/src/App.js
+++ 5-Component/4-Data flow: Props & States definitions & difference/src/App.js
@@ -1,5 +1,6 @@
 import Car from "./components/Car";
 import { useState } from "react";
+//your first need to import something extra from React (useState)
 let carsData = [
   { brand: "BMW", model: "X5", img: "./images/bmw-x5.png" },
   { brand: "Ford", model: "Galaxy", img: "./images/ford-galaxy.jpeg" },
@@ -7,30 +8,41 @@ let carsData = [
 ];
 
 function App() {
-  const [cars, setCars] = useState(carsData);
+  // we declare an array with two items in it:
+  let [cars, setCars] = useState(carsData);
+  // cars => The first item should be a variable you want your component to "keep track of"
+  // setCars ==> e second item should be a function which will let us change the variable to a different value
 
   const onClear = () => {
-    console.log("Clear cars!");
-    //cars = [];
+    // cars = []; // update our app using traditional variables, it won't change in the browser
     setCars([]);
+  };
+
+  const onBringCars = () => {
+    setCars(carsData);
   };
 
   return (
     <div className="App">
       <h1> React Props and State</h1>
-      {cars.map((element, index) => (
-        <Car
-          key={index}
-          brand={element.brand}
-          model={element.model}
-          img={element.img}
-        />
-      ))}
 
-      {/* <Car brand="BMW" model="X5" img="./images/bmw-x5.png" />
-   <Car brand="Ford" model="Galaxy" img="./images/ford-galaxy.jpeg" />
-   <Car brand="Kia" model="Sportage" img="./images/kia-sportage.jpg" /> */}
+      {cars.map((car, index) => {
+        return (
+          <Car
+            key={index}
+            model={car.model}
+            imageData={car.img}
+            brand={car.brand}
+          />
+        );
+      })}
+
+      {/* <Car brand="BMW" model="X5" imageData="./images/bmw-x5.png" />
+   <Car brand="Ford" model="Galaxy" imageData="./images/ford-galaxy.jpeg" />
+   <Car brand="Kia" model="Sportage" imimageDatag="./images/kia-sportage.jpg" /> */}
+
       <button onClick={onClear}>Clear</button>
+      <button onClick={onBringCars}>Bring the cars</button>
     </div>
   );
 }
