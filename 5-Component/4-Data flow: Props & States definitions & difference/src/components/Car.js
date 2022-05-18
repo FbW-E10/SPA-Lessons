9b4c8f2f5d1eaf905cbe5cbdc74fab272c9db6e9
Ghassan Aldarwish <ghassanaldarwish@gmail.com>
2022-05-18 11:59:08 +0200

    Update Car.js

--- 5-Component/4-Data flow: Props & States definitions & difference/src/components/Car.js
+++ 5-Component/4-Data flow: Props & States definitions & difference/src/components/Car.js
@@ -1,9 +1,9 @@
-function Car(props){
+function Car({ brand, model, imageData }){
 return (
     <div className ="car" >
-        <h3> Brand : {props.brand}</h3>
-        <h3> Car Model: {props.model}</h3>
-        <img src={props.img}  />
+       <h3> Brand : {brand}</h3>
+      <h3> Car Model: {model}</h3>
+      <img src={imageData} alt={brand} />
     </div>
 
 )
