1dc7945c090077bd4008e4b59ed9e4635c0f09da
Ghassan Aldarwish <ghassanaldarwish@gmail.com>
2022-05-31 13:14:47 +0200

    Update Navbar.jsx

--- 6-Router/1-Introduction: Routing in a Single Page Application with React Router/src/components/Navbar/Navbar.jsx
+++ 6-Router/1-Introduction: Routing in a Single Page Application with React Router/src/components/Navbar/Navbar.jsx
@@ -8,15 +8,15 @@ export default function Navbar() {
         <Link className="navbar-brand" to="/">
           Navbar
         </Link>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link className="nav-link" to="/">
+        <div className="container" id="navbarNavAltMarkup">
+          <div className="d-flex ">
+            <Link className="nav-link mx-4" to="/">
               Home
             </Link>
-            <Link className="nav-link" to="/about">
+            <Link className="nav-link mx-4" to="/about">
               About
             </Link>
-            <Link className="nav-link" to="/contact">
+            <Link className="nav-link mx-4" to="/contact">
               Contact
             </Link>
           </div>
